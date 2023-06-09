50c294f08f5370bd15f1b8d9c1b5620b4335cf53
Emdadul_Islam <emdadulislam439@gmail.com>
2023-06-09 12:13:40 +0600

    finished task 01: added input field and showed length && modified readme.md file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputLength, setInputLength] = useState(0);
+  const changeInputHandler = (event) => {
+    setInputLength(event.target.value.length);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="textInput" onChange={changeInputHandler} />
+      <p>Length is: {inputLength} characters</p>
     </div>
   );
 }
